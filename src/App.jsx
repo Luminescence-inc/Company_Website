db0e5822dc62d9df9309357b2220f42a7b4dfab6
walterokpala <uchewalter8@gmail.com>
2023-06-13 22:07:01 -0400

    added the website

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import style from "./style";
+import {
+  Billing,
+  Business,
+  CardDeal,
+  Clients,
+  CTA,
+  Footer,
+  Hero,
+  Navbar,
+  Stats,
+  Testimonials,
+} from "./components";
+
+const App = () => (
+  <div className="bg-primary  w-full overflow-hidden">
+    <div className={` ${style.paddingX}  ${style.flexCenter}`}>
+      {/* Navigation */}
+      <div className={`${style.boxWidth}`}>
+        <Navbar />
+      </div>
+    </div>
+    {/* Hero */}
+    <div className={`bg-primary ${style.flexStart}`}>
+      <div className={`${style.boxWidth}`}>
+        <Hero />
+      </div>
+    </div>
+    {/* Body */}
+    <div className={`bg-primary ${style.paddingX} ${style.flexStart}`}>
+      <div className={`${style.boxWidth}`}>
+        <Stats />
+        <Business />
+        <Billing />
+        <CardDeal /> <Clients /> <CTA />
+        <Footer />
+        <Testimonials />
+      </div>
+    </div>
+  </div>
+);
+
+export default App;
