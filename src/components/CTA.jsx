cdee1da3c31cc8c1185fba71fcd8982c2e8ea7bc
walterokpala <uchewalter8@gmail.com>
2023-06-29 08:53:46 -0400

    changed the information section

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -13,10 +13,7 @@ const CTA = () => (
     <div className={layout.sectionInfo}>
       <h2 className={styles.heading2}>Home Explorer</h2>
       <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-        BizConnect is a directory of businesses and services rendered by a
-        particular group of people based on their country of origin and current
-        location.This product be able to connect you with people from your
-        country who offer a particular service around your location
+        Rethinking the business of real estate across the globe
       </p>
     </div>
   </section>
--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -7,14 +7,11 @@ const CardDeal = () => (
     <div className={layout.sectionInfo}>
       <h2 className={styles.heading2}>Ripple</h2>
       <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-        A universal solution where DJs can sign up, upload their availability,
-        and snips from their previous events and then they can be booked by
-        anyone in their locale or around the world. We want Djs to be universal
-        such that DJs can go viral on the platform due to their mixes and the
-        sounds they generate.
+        ReStrategizing and repositioning event/Talent management using new
+        thinking and new technology.
       </p>
     </div>
-    <div className={`${layout.sectionImg} w-[100%]`}>
+    <div className={`${layout.sectionImg} w-[100%] relative`}>
       <img src={Ripple} alt="card" className=" object-contain w-[30rem] " />
     </div>
   </section>
